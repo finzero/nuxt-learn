8760a78c413a8ec0981b4ab199aa20901895a5e8
Fazrin <fazrin.mutaqin@wgs-id.com>
2022-09-03 14:40:23 +0700

    Initial Commit

---
+++ app.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    Faz apps, implement vue select    
+    <v-select
+    v-model="selected"
+    :options="kampus"
+    :reduce="(item) => item.name"
+    label="name"
+  ></v-select>
+
+  yang dipilih: {{ selected }}
+
+  </div>
+</template>
+
+<script>
+  import vSelect from 'vue-select';
+  import 'vue-select/dist/vue-select.css';
+  export default {
+    components: [vSelect],
+    data() {
+      return {
+        selected: null,
+        kampus: [
+          {code: 'lpkia', name: 'LPKIA'},
+          {code: 'unisba', name: 'Unisba'},
+          {code: 'itb', name: 'ITB'},
+          {code: 'upi', name: 'UPI'},
+          {code: 'stttelkom', name: 'STT Telkom'},
+        ]
+      }
+    }
+  }
+  </script>
---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+import { defineNuxtConfig } from 'nuxt';
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  css: ['vue-select/dist/vue-select.css'],
+});
