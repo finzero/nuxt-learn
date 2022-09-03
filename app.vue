72ea994223a87b4a724bbc04b8e6850e1e4d9694
Fazrin <fazrin.mutaqin@wgs-id.com>
2022-09-03 14:47:12 +0700

    Remove unessesary imports

--- app.vue
+++ app.vue
@@ -15,10 +15,7 @@
 </template>
 
 <script>
-  import vSelect from 'vue-select';
-  import 'vue-select/dist/vue-select.css';
   export default {
-    components: [vSelect],
     data() {
       return {
         selected: null,
